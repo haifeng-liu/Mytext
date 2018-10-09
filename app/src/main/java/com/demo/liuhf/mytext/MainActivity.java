73a97c089f960a9c9e5bf8e8c902a621829da061
haifeng-liu <1695656786@qq.com>
2018-10-09 12:26:34 +0800

    优化viewholder

--- app/src/main/java/com/demo/liuhf/mytext/MainActivity.java
+++ app/src/main/java/com/demo/liuhf/mytext/MainActivity.java
@@ -1,27 +1,46 @@
 package com.demo.liuhf.mytext;
 
 import android.app.Activity;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.demo.liuhf.mybaselibrarys.base.BaseActivity;
+import com.demo.liuhf.mybaselibrarys.base.BaseAdapter;
 import com.demo.liuhf.mybaselibrarys.base.BaseGuideActivity;
+import com.demo.liuhf.mybaselibrarys.base.BaseHolder;
+import com.demo.liuhf.mytext.databinding.ActivityMainBinding;
+import com.demo.liuhf.mytext.databinding.ItemBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public  class MainActivity extends BaseActivity {
 
 
+    ActivityMainBinding binding;
+    MyAdapter adapter;
+    List<String> data=new ArrayList<>();
     @Override
     public void initview() {
+        data.add("11111");
+        data.add("12222");
+        data.add("13333");
+        data.add("14444");
 
+        adapter=new MyAdapter(this,data,R.layout.item);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        binding.recycler.setAdapter(adapter);
     }
 
     @Override
     public View onCreateview() {
-        return null;
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        return binding.getRoot();
     }
 
     @Override
@@ -38,5 +57,14 @@ public  class MainActivity extends BaseActivity {
             firstPressedTime=System.currentTimeMillis();
         }
     }
-
+    class  MyAdapter extends BaseAdapter<String>{
+        public MyAdapter(Context context, List<String> data, int layoutId) {
+            super(context, data,layoutId);
+        }
+        @Override
+        public void onBind(BaseHolder holder, String s, int position) {
+            ItemBinding ibinding= (ItemBinding) holder.getBinding();
+            ibinding.tvName.setText(s);
+        }
+    }
 }
--- mybaselibrarys/src/main/java/com/demo/liuhf/mybaselibrarys/base/BaseHolder.java
+++ mybaselibrarys/src/main/java/com/demo/liuhf/mybaselibrarys/base/BaseHolder.java
@@ -1,6 +1,8 @@
 package com.demo.liuhf.mybaselibrarys.base;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
@@ -14,9 +16,16 @@ import android.view.ViewGroup;
  * 修改历史：
  */
 
-public class BaseHolder extends RecyclerView.ViewHolder {
+public class BaseHolder<B extends ViewDataBinding> extends RecyclerView.ViewHolder {
+    B binding;
     View itemView;
     SparseArray<View> views;//存放Item中的控件
+    public BaseHolder(B binding){
+        super(binding.getRoot());
+        this.binding = binding;
+        views = new SparseArray<View>();
+    }
+
     public BaseHolder(View itemView){
         super(itemView);
         this.itemView = itemView;
@@ -24,8 +33,10 @@ public class BaseHolder extends RecyclerView.ViewHolder {
     }
     //供adapter调用，返回holder
     public static <T extends BaseHolder> T getHolder(Context cxt, ViewGroup parent, int layoutId){
-        return (T) new BaseHolder(LayoutInflater.from(cxt).inflate(layoutId, parent, false));
+        return (T) new BaseHolder(DataBindingUtil.inflate(LayoutInflater.from(cxt),layoutId,parent, false));
     }
+
+
     //根据Item中的控件Id获取控件
     public <T extends View> T getView(int viewId){
         View childreView = views.get(viewId);
@@ -35,6 +46,12 @@ public class BaseHolder extends RecyclerView.ViewHolder {
         }
         return (T) childreView;
     }
+
+    //返回binding
+    public B getBinding() {
+        return binding;
+    }
+
     //根据Item中的控件Id向控件添加事件监听
     public BaseHolder setOnClickListener(int viewId, View.OnClickListener listener){
         View view = getView(viewId);
